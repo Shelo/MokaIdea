82c60cf574bdba21ec910dace3853e52b1beec01
Rodolfo Castillo Mateluna <rodolfocastillomateluna@gmail.com>
2015-02-10 23:41:36 +0100

    Optimize IdeaListFragment.IdeaAdapter getView

--- app/src/main/java/com/mokadev/mokaidea/IdeaListFragment.java
+++ app/src/main/java/com/mokadev/mokaidea/IdeaListFragment.java
@@ -18,28 +18,39 @@ public class IdeaListFragment extends ListFragment {
 	}
 
 	private class IdeaAdapter extends ArrayAdapter<Idea> {
+        private class ViewHolder {
+            TextView title;
+            TextView desc;
+            CheckBox upVote;
+        }
+
 		public IdeaAdapter(ArrayList<Idea> ideas) {
 			super(getActivity(), android.R.layout.simple_list_item_1, ideas);
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+
 			// if the view is first asked, it will be null, if so, we should inflate the xml.
-			if(convertView == null)
-				convertView = getActivity().getLayoutInflater().inflate(R.layout.idea_item, null);
+			if(convertView == null) {
+                holder = new ViewHolder();
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.idea_item, null);
+                holder.title = (TextView) convertView.findViewById(R.id.idea_item_title);
+                holder.desc = (TextView) convertView.findViewById(R.id.idea_item_short_desc);
+                holder.upVote = (CheckBox) convertView.findViewById(R.id.idea_item_up_vote);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
 
 			// get idea at that position.
 			Idea idea = getItem(position);
 
-			// get view fields.
-			TextView title = (TextView) convertView.findViewById(R.id.idea_item_title);
-			TextView desc = (TextView) convertView.findViewById(R.id.idea_item_short_desc);
-			CheckBox upVote = (CheckBox) convertView.findViewById(R.id.idea_item_up_vote);
-
 			// fill view fields.
-			title.setText(idea.getTitle());
-			desc.setText(idea.getShortDescription());
-			upVote.setChecked(idea.isUpVoted());
+			holder.title.setText(idea.getTitle());
+			holder.desc.setText(idea.getShortDescription());
+			holder.upVote.setChecked(idea.isUpVoted());
 
 			return convertView;
 		}
