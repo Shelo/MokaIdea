09a2bd5c4d967171eb0c032c6cff56ddedb9f30f
Shelo <marjara35@gmail.com>
2015-02-10 20:21:26 -0400

    Add toast.

--- app/src/main/java/com/mokadev/mokaidea/LoginActivity.java
+++ app/src/main/java/com/mokadev/mokaidea/LoginActivity.java
@@ -1,17 +1,28 @@
 package com.mokadev.mokaidea;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		ImageButton button = (ImageButton) findViewById(R.id.login_google);
+
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(LoginActivity.this, R.string.toast_string, Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 }
